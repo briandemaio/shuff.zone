f753c3aeec64d1ec5f9bdad6bbe6a0094ba0c32b
Brian DeMaio <briguyd@gmail.com>
2024-06-14 18:05:22 -0500

    angular 15 upgrade

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,10 @@ import { AppComponent } from './app.component';
 import { CourtComponent } from './court/court.component';
 import { ActionsBarComponent } from './actions-bar/actions-bar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
+import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatIconModule } from '@angular/material/icon';
-import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatLegacyTooltipModule as MatTooltipModule } from '@angular/material/legacy-tooltip';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
